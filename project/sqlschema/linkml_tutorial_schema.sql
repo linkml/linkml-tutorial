57518b0f86c45ed2bba2adb326a5ae304cc5bfd1
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-09-21 13:49:07 -0700

    add regened project

--- project/sqlschema/linkml_tutorial_schema.sql
+++ project/sqlschema/linkml_tutorial_schema.sql
@@ -1,5 +1,20 @@
 
 
+CREATE TABLE "Person" (
+	id TEXT NOT NULL, 
+	name TEXT, 
+	birth_date DATE, 
+	age_in_years INTEGER, 
+	primary_email TEXT, 
+	vital_status TEXT NOT NULL, 
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE "PersonCollection" (
+	entries TEXT, 
+	PRIMARY KEY (entries)
+);
+
 CREATE TABLE "Animal" (
 	id TEXT NOT NULL, 
 	name TEXT, 
@@ -9,32 +24,7 @@ CREATE TABLE "Animal" (
 	breed TEXT, 
 	color TEXT, 
 	weight_in_mgs TEXT, 
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE "AnimalCollection" (
-	entries TEXT, 
-	animals TEXT, 
-	PRIMARY KEY (entries, animals)
-);
-
-CREATE TABLE "Collection" (
-	entries TEXT, 
-	PRIMARY KEY (entries)
-);
-
-CREATE TABLE "Person" (
-	id TEXT NOT NULL, 
-	name TEXT, 
-	birth_date DATE, 
-	age_in_years INTEGER, 
-	primary_email TEXT, 
-	vital_status TEXT NOT NULL, 
-	pets TEXT, 
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE "PersonCollection" (
-	entries TEXT, 
-	PRIMARY KEY (entries)
+	"Person_id" TEXT, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY("Person_id") REFERENCES "Person" (id)
 );
